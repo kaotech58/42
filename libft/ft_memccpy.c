1396bb8974e9c90fb9feec7f57de70b80c8f6d79
Timtohy Eiffe <kaotech@kat.fritz.box>
2021-09-22 15:35:46 +0930

    updated

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -1,22 +1,22 @@
 #include "libft.h"
 
-void	*ft_memccpy(void *dst, const void *src, int c, size_t len)
+void	*ft_memccpy(void *dst, const void *src, int c, size_t n)
 {
-	size_t	i;
+	size_t			i;
+	unsigned char	*ptr_dst;
+	unsigned char	*ptr_src;
 
+	ptr_dst = (unsigned char*)dst;
+	ptr_src = (unsigned char*)src;
 	i = 0;
-	if (!dst || !src)
+	while (i < n)
 	{
-		return (NULL);
-	}
-	while (i < len)
-	{
-		*(unsigned char*)(dst + i) = *(unsigned char*)(src + i);
-		if (*(unsigned char*)(src + i) == (unsigned char)c);
+		ptr_dst[i] = ptr_src[i];
+		if (ptr_dst[i] == (unsigned char)c)
 		{
-			return (dst + i + 1);
+			return ((void*)(dst + i + 1));
 		}
-		i++;
+		++i;
 	}
 	return (NULL);
 }
