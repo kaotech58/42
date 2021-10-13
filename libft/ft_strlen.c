3adebaa5be9c1cf3cefe51e792fa7777b8d3c710
Timothy Eiffe <timothy@kaotech.co>
2021-10-13 16:50:36 +1030

    added comments

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,12 +6,15 @@
 /*   By: teiffe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/08 13:26:05 by teiffe            #+#    #+#             */
-/*   Updated: 2021/10/13 14:34:52 by teiffe           ###   ########.fr       */
+/*   Updated: 2021/10/13 15:34:47 by teiffe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+/*this function creates a variable then keeps incrementing its value
+ * until it gets to the null terminator it then gets the end value
+ * and subtracts the starting value to show the length of the string*/
 size_t	ft_strlen(const char *s)
 {
 	const char	*stringend = s;
